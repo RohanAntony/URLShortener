187de31cf21ca75b8919cde43074f420de189f6e
Rohan Antony <rohan271995@gmail.com>
2018-09-25 07:54:40 +0530

    Cleaning up repo. This repo will only serve as backend with API endpoints to perform the shorteneing information

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ Website to shorten any given URL.
   - Installed automatically when installing nodejs
 - Mongodb >= 3.2
   - To install `apt-get install mongodb`
+
+# Application
+- First clone the repository using `git clone`
+- Run `npm install` inside the root directory to install the dependencies
+- To start application `npm start`
+- Go to http://localhost:3000
+
+# Implementation
+- To Shorten URL, send a request to endpoing `/shorten` with 'url' as field in the post body
+- The returned value will be the reference for the url
+
+# ToDo
+- Implement a GET method along with the POST API to shorten the URL.
+- Generate only a backend API on this repo
--- routes/index.js
+++ routes/index.js
@@ -5,98 +5,95 @@ var mongoose = require('mongoose');
 var URLStore = require('../models/URLStore.js');
 
 var SIZE_OF_STRING = 7;
-var numberOfGifs = 5;
 
 var table = {};
 
-function makeid(cb){
+
+let makeid = () => {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for( var i=0; i < SIZE_OF_STRING; i++ )
         text += possible.charAt(Math.floor(Math.random() * possible.length));
-    URLStore.where({shortened:text})
-    .findOne(function(err,obj){
-      if(err)
-        console.log('Error performing database operation')
-      if(obj){
-        return makeid(cb);
-      }else{
-        cb(text);
-      }
-    })
+    // URLStore.where({shortened:text})
+    // .findOne(function(err,obj){
+    //   if(err)
+    //     console.log('Error performing database operation')
+    //   if(obj){
+    //     return makeid(cb);
+    //   }else{
+    //     cb(text);
+    //   }
+    // })
+    // Move this code out of MakeId as it is part of write operation to DB
 }
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
-});
-
 router.post('/shorten',function(req,res){
-  makeid(function(shortUrl){
-    var addUrl = new URLStore({url:req.body.url,shortened:shortUrl,visits:0,details:[]});
-    addUrl.save(function(err){
-      if(err)
-        console.log(err);
-      res.json({url:req.body.url,shortened:shortUrl});
-    });
-  });
-})
-
-router.get('/pageNotFound',function(req,res){
-  res.redirect('/');
-})
-
-router.get('/pageNotFound/:id',function(req,res){
-  res.render('pageNotFound',{randomValue:parseInt(Math.random()*numberOfGifs),shortenedUrl:'http://localhost:3000/'+req.params.id});
+  let shortenedURL
+  shortenedURL = makeid();
+  // function(shortUrl){
+  //   var addUrl = new URLStore({url:req.body.url,shortened:shortUrl,visits:0,details:[]});
+  //   addUrl.save(function(err){
+  //     if(err)
+  //       console.log(err);
+  //     res.json({url:req.body.url,shortened:shortUrl});
+  //   });
+  // }
+  // Implement a addUrlToDB seperately and call it here
 })
 
 router.get('/info/:id',function(req,res){
-  URLStore.where({shortened:req.params.id}).find(function(err,obj){
-    if(err){
-      console.log('Error performing database operation');
-      res.send('Unable to access details');
-    }
-    if(obj.length != 0){
-      res.render('details',{urlObject:obj[0]});
-    }else{
-      res.redirect('/pageNotFound/'+req.params.id)
-    }
-  })
+  // URLStore.where({shortened:req.params.id}).find(function(err,obj){
+  //   if(err){
+  //     console.log('Error performing database operation');
+  //     res.send('Unable to access details');
+  //   }
+  //   if(obj.length != 0){
+  //     res.render('details',{urlObject:obj[0]});
+  //   }else{
+  //     res.redirect('/pageNotFound/'+req.params.id)
+  //   }
+  // })
+  // Implement a get URL info function which only fetches it from DB
+  // Use this function here and convert data to readable format and send it
 })
 
 router.get('/:id',function(req,res){
-  console.log(req.params.id);
-  var id = req.params.id;
-  URLStore.where({shortened:req.params.id}).find(function(err,obj){
-    if(err){
-      console.log('Error performing database operation');
-      res.send('Unable to query db');
-    }
-    if(obj.length != 0){
-      console.log('redirecting to '+obj[0].url);
-      var userDetails = {
-        ip:req.ip,
-        ips:req.ips,
-        time:new Date(),
-        headers:{
-          'user-agent':req.get('user-agent'),
-          accept:req.get('accept'),
-          'accept-language':req.get('accept-language'),
-          'accept-encoding':req.get('accept-encoding'),
-          cookie:req.get('cookie'),
-          connection:req.get('connection'),
-          'upgrade-insecure-requests':req.get('upgrade-insecure-requests')
-        }
-      }
-      console.log(req.headers);
-      obj[0].details.push(userDetails);
-      obj[0].visits++;
-      obj[0].save();
-      res.redirect(obj[0].url);
-    }else{
-      res.redirect('/pageNotFound/'+req.params.id)
-    }
-  })
+  // console.log(req.params.id);
+  // var id = req.params.id;
+  // URLStore.where({shortened:req.params.id}).find(function(err,obj){
+  //   if(err){
+  //     console.log('Error performing database operation');
+  //     res.send('Unable to query db');
+  //   }
+  //   if(obj.length != 0){
+  //     console.log('redirecting to '+obj[0].url);
+  //     var userDetails = {
+  //       ip:req.ip,
+  //       ips:req.ips,
+  //       time:new Date(),
+  //       headers:{
+  //         'user-agent':req.get('user-agent'),
+  //         accept:req.get('accept'),
+  //         'accept-language':req.get('accept-language'),
+  //         'accept-encoding':req.get('accept-encoding'),
+  //         cookie:req.get('cookie'),
+  //         connection:req.get('connection'),
+  //         'upgrade-insecure-requests':req.get('upgrade-insecure-requests')
+  //       }
+  //     }
+  //     console.log(req.headers);
+  //     obj[0].details.push(userDetails);
+  //     obj[0].visits++;
+  //     obj[0].save();
+  //     res.redirect(obj[0].url);
+  //   }else{
+  //     res.redirect('/pageNotFound/'+req.params.id)
+  //   }
+  // })
+  // Implement a function to get redirectedURL for shortenedURL
+  // Use the above fetch URL info function to fetch the details about the URL
+  // Change the properties of the URL info urlObject
+  // Implement a function which updates the URL info object and call it with the new URL object
 })
 
 module.exports = router;
